a88b9b91fedf30a12c7a7f9709a54e13d49d6348
Maryum Mehmood <maryummehmood650852@gmail.com>
2026-09-18 03:20:20 +0500

    Temporarily disable Zephyr reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,11 +12,7 @@ export default defineConfig({
 
   reporter: [
     ['html'],
-    ['playwright-zephyr', {
-      host: 'https://maryummehmood650852.atlassian.net/',
-      authorizationToken: 'ATATT3xFfGF0c4i4j7abm4bBqUYbmcYMlGSIz9wxGTLO-90SPBPMKbba8E9Yhcre6HZsDp3Zv7PgmG8KeJ6DKpC8t9IfwqKzZd386be6qDDKyoWjFVwS71LUPb-asAeWBZ_7fUgnvlm3wejpu_loZkGywzTR3fm0Mn0fR2OF_gi0ImOTuH_E_1M=97CAE704', // 👈 Yahan apna lamba API token paste karein
-      projectKey: 'SCRUM', 
-    }]
+    
   ],
 
   use: {
