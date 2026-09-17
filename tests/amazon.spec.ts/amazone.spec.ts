98ec8f513d7c8ad015078d8d8767f6a2e850a831
Maryum Mehmood <maryummehmood650852@gmail.com>
2026-09-17 12:55:41 +0500

    remove seed spec file to fix pipeline

--- tests/amazon.spec.ts/amazone.spec.ts
+++ tests/amazon.spec.ts/amazone.spec.ts
@@ -5,7 +5,6 @@ test('test', async ({ page }) => {
   
   await page.getByRole('link', { name: 'Hello, sign in Account & Lists' }).click();
   await page.getByRole('textbox', { name: 'Enter mobile number or email' }).click();
-  await page.locator('#claim-input-container').click();
   await page.getByRole('textbox', { name: 'Enter mobile number or email' }).fill('maryummehmood900@gmail.com');
   await page.getByRole('button', { name: 'Continue' }).click();
   await page.getByRole('textbox', { name: 'Password' }).click();
--- tests/seed.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test.describe('Test group', () => {
-  test('seed', async ({ page }) => {
-    // generate code here.
-  //Open amazon cart, click 'Go to Cart' button, then open login page and fill email field.
-  });
-});
