30683e72010e070b969bd757b35a6bfdd0a6325d
Maryum Mehmood <maryummehmood650852@gmail.com>
2026-09-17 05:46:56 +0500

    fix: remove duplicate test step and map jira token cleanly

--- tests/w3schooldemo.spec.ts/w3school.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Open W3Schools Homepage', async ({ page }) => {
-  await page.goto('https://www.w3schools.com/');
-
-  await expect(page).toHaveTitle(/W3Schools/i);
-});
